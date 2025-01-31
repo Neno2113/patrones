8471651e76b8f94bccac9dca60942ff7ad47d1ad
Anel <anelgabriel002@gmail.com>
2025-01-30 20:01:45 -0400

    first Commit

---
+++ 01-builder.ts
@@ -0,0 +1,96 @@
+/**
+ * ! Patrón Builder:
+ * Es un patrón de diseño creacional que nos permite construir objetos complejos
+ * paso a paso.
+ *
+ * El patrón nos permite producir distintos tipos y representaciones
+ * de un objeto empleando el mismo código de construcción.
+ *
+ * * Es útil cuando necesitamos construir un objeto complejo con muchas partes
+ * * y queremos que el proceso de construcción sea independiente de las partes
+ * * que lo componen.
+ *
+ * https://refactoring.guru/es/design-patterns/builder
+ */
+
+import { COLORS } from "../helpers/colors.ts";
+
+
+
+class Computer {
+    public cpu: string = 'cpu - not defined';
+    public ram: string = 'ram - not defined';
+    public storage: string = 'storage - not defined';
+    public gpu?: string;
+
+
+    displayConfiguration(){
+        console.log(`Configuracion de la computadora:
+        - CPU: ${this.cpu}
+        - RAM: ${this.ram}
+        - Almacenamiento: ${this.storage}
+        - GPU: ${this.gpu ?? 'No tiene GPU'}    
+        `);
+    }
+}
+
+
+
+
+class ComputerBuilder {
+    private computer: Computer = new Computer();
+
+    constructor(){
+        this.computer = new Computer();
+    }
+
+    setCPU(cpu: string): ComputerBuilder {
+        this.computer.cpu = cpu;
+        return this;
+    }
+
+    setRam(ram: string): ComputerBuilder {
+        this.computer.ram = ram;
+        return this;
+    }
+
+    setStorage(storage: string): ComputerBuilder {
+        this.computer.storage = storage;
+        return this;
+    }
+    
+    setGpu(gpu: string): ComputerBuilder {
+        this.computer.gpu = gpu;
+        return this;
+    }
+
+    build(){
+        return this.computer;
+    }
+}
+
+
+function main(){
+    const computer = new ComputerBuilder()
+        .setCPU('Intel core 2 duo')
+        .setRam('8GB')
+        .setStorage('256GB')
+        .build();
+
+    console.log('%cComputadora basica: ', COLORS.red);
+    computer.displayConfiguration();
+    
+    const lenogoLegion5 = new ComputerBuilder()
+        .setCPU('AMD Ryzen 7')
+        .setRam('32GB')
+        .setStorage('2TB')
+        .setGpu('Nvidia RTX 3060')
+        .build();
+
+    console.log('%cLenovo Legion 5: ', COLORS.violet);
+    lenogoLegion5.displayConfiguration();
+    
+}
+
+
+main();
