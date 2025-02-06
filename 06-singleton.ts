1a8cf793e3d353a84ba7eef972ed3689151c45b8
Anel <anelgabriel002@gmail.com>
2025-02-05 20:16:20 -0400

    Add CodeEditor and Player classes with state management; implement Singleton pattern for DragonBalls and DatabaseConnection

--- 06-singleton.ts
+++ 06-singleton.ts
@@ -8,4 +8,78 @@
  * * objeto de configuración.
  *
  * https://refactoring.guru/es/design-patterns/singleton
- */
+*/
+
+import { COLORS } from "../helpers/colors.ts";
+
+
+
+
+class DragonBalls {
+
+    private static instance: DragonBalls;
+    private ballsCollected: number = 0;
+
+
+    private constructor() { 
+        this.ballsCollected = 0;
+    }
+
+    public static getInstance(): DragonBalls {
+        if(!DragonBalls.instance) {
+            DragonBalls.instance = new DragonBalls();
+            console.log('%cSe ha creado una instancia de DragonBalls', COLORS.cyan);
+            
+        }
+
+        return DragonBalls.instance;
+    }
+
+    collectBall() {
+        if( this.ballsCollected < 7 ) {
+            this.ballsCollected++;
+            console.log(`%cBola recolectada: ${this.ballsCollected}`, COLORS.blue);
+            return;
+        }
+
+        console.log('%cYa tienes las 7 bolas de dragón', COLORS.red);
+        
+    }
+
+    summonShenron() {
+        if(this.ballsCollected === 7) {
+            console.log('%cHas invocado a Shenron', COLORS.green);
+            this.ballsCollected = 0;
+            return;
+        }
+
+        console.log('%cNo puedes invocar a Shenron', COLORS.red);
+    }
+
+}
+
+
+
+function main(){
+    const goku = DragonBalls.getInstance();
+    
+    goku.collectBall();
+    goku.collectBall();
+    goku.collectBall();
+    
+    goku.summonShenron();
+
+    const vegeta = DragonBalls.getInstance();
+    vegeta.collectBall();
+    vegeta.collectBall();
+    vegeta.collectBall();
+    vegeta.collectBall(); 
+
+    goku.summonShenron();
+    vegeta.summonShenron();
+
+}
+
+
+main();
+
